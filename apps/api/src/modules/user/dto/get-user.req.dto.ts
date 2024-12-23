a558d872cc5c6f370814fe8bcaa985cf912f9ced
Zaehyeon2 <insam2802@gmail.com>
2024-12-23 13:40:29 +0900

    user my info api 추가

--- apps/api/src/modules/user/dto/get-user.req.dto.ts
+++ apps/api/src/modules/user/dto/get-user.req.dto.ts
@@ -1,11 +1,8 @@
 import { createZodDto } from '@anatine/zod-nestjs';
 import { userSchema } from 'apps/api/src/modules/user/user.interface';
 
-export class GetUserReqDto extends createZodDto(
-  userSchema
-    .pick({
-      id: true,
-      phoneNumber: true,
-    })
-    .partial(),
+export class GetUserByPhoneReqDto extends createZodDto(
+  userSchema.pick({
+    phoneNumber: true,
+  }),
 ) {}
--- apps/api/src/modules/user/user.controller.ts
+++ apps/api/src/modules/user/user.controller.ts
@@ -8,7 +8,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
-import { GetUserReqDto, GetUserResDto, UpdateUserDto } from './dto';
+import { GetUserByPhoneReqDto, GetUserResDto, UpdateUserDto } from './dto';
 import { UserService } from './user.service';
 import { MaintenanceGuard, MaintenanceType } from '../maintenance';
 import { User } from './user.interface';
@@ -37,18 +37,17 @@ export class UserController {
     return safeParseOrThrow(UpdateUserDto.zodSchema.strict(), updateUserParams);
   }
 
-  @Get()
-  @ApiOperation({ summary: 'Get a user by id or phoneNumber' })
-  async getUserByIdOrPhoneNumber(@Query() query: GetUserReqDto): Promise<GetUserResDto> {
-    let user: User | null = null;
+  @Get('my-info')
+  @ApiOperation({ summary: 'Get my info' })
+  @UserGuard()
+  async getMyInfo(@UserParam() user: User): Promise<GetUserResDto> {
+    return safeParseOrThrow(GetUserResDto.zodSchema.strict(), user);
+  }
 
-    if (query.id) {
-      user = await this.userService.getUserById(query.id);
-    } else if (query.phoneNumber) {
-      user = await this.userService.getUserByPhoneNumber(query.phoneNumber);
-    } else {
-      throw new BadRequestException('id or phoneNumber is required');
-    }
+  @Get()
+  @ApiOperation({ summary: 'Get a user by phoneNumber' })
+  async getUserByPhoneNumber(@Query() query: GetUserByPhoneReqDto): Promise<GetUserResDto> {
+    const user = await this.userService.getUserByPhoneNumber(query.phoneNumber);
 
     if (!user) {
       throw new NotFoundException('User not found');
